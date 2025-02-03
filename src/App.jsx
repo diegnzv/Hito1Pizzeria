58d4a06be4d1ea0e3facf3a3aeb3785cfe9b1f89
Diego Gonzalez V <diegog.vielma1@gmail.com>
2025-02-02 21:13:13 -0300

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './components/Home.jsx'
+
+
+const App = () =>{
+
+
+  return(
+    <>
+    <Navbar/>
+    <Home/>
+    <Footer/>
+    </>
+
+  )
+
+}
+
+export default App
